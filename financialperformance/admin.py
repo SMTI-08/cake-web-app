225a6bebde3f5f7cb5eebec58dd5eb956eee4b2d
muhammadnizaryp <muhammadnizaryp@gmail.com>
2019-01-03 00:54:23 +0700

    add database models

--- financialperformance/admin.py
+++ financialperformance/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import SektorPerusahaan, Perusahaan, LaporanKeuangan
 
 # Register your models here.
+
+class SektorPerusahaanAdmin(admin.ModelAdmin):
+    list_display = ['kd_sektor','nama_sektor','deskripsi_sektor']
+    list_per_page = 25
+admin.site.register(SektorPerusahaan, SektorPerusahaanAdmin)
+
+class PerusahaanAdmin(admin.ModelAdmin):
+    list_display = ['kd_sektor','kd_perusahaan','nama_perusahaan','deskripsi_perusahaan']
+    list_per_page = 25
+admin.site.register(Perusahaan, PerusahaanAdmin)
+
+class LaporanKeuanganAdmin(admin.ModelAdmin):
+    list_display = ['kd_perusahaan','year','cash','inventory','current_asset','fixed_asset',
+                    'total_asset','current_liabilities','long_term_liabilities','total_liabilities',
+                    'outstanding_share','total_equity','total_revenue','gross_profit','operating_profit',
+                    'interest_expense','net_profit','earnings_per_share','dividend_payment','stock_price',
+                    'kurs_rupiah_dollar']
+    list_per_page = 25
+admin.site.register(LaporanKeuangan, LaporanKeuanganAdmin)
--- financialperformance/models.py
+++ financialperformance/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
 # Create your models here.
+
+class SektorPerusahaan(models.Model):
+    kd_sektor = models.CharField(primary_key=True, max_length=10)
+    nama_sektor = models.CharField(max_length=50)
+    deskripsi_sektor = models.TextField()
+
+    def __str__(self):
+        return '%s %s'%(self.kd_sektor, self.nama_sektor)
+
+class Perusahaan(models.Model):
+    kd_sektor = models.ForeignKey(SektorPerusahaan, related_name='fk_sektor_perusahaan', on_delete=models.CASCADE)
+    kd_perusahaan = models.CharField(primary_key=True, max_length=10)
+    nama_perusahaan = models.CharField(max_length=50)
+    deskripsi_perusahaan = models.TextField()
+
+    def __str__(self):
+        return '%s %s'%(self.kd_perusahaan, self.nama_perusahaan)
+
+class LaporanKeuangan(models.Model):
+    kd_perusahaan = models.ForeignKey(Perusahaan, related_name='fk_perusahaan', on_delete=models.CASCADE)
+    year = models.CharField(max_length=4, choices=(('2017','2017'), ('2016','2016'), ('2015','2015'), ('2014','2014'), ('2013','2013')))
+    cash = models.BigIntegerField()
+    inventory = models.BigIntegerField()
+    current_asset = models.BigIntegerField()
+    fixed_asset = models.BigIntegerField()
+    total_asset = models.BigIntegerField()
+    current_liabilities = models.BigIntegerField()
+    long_term_liabilities = models.BigIntegerField()
+    total_liabilities = models.BigIntegerField()
+    outstanding_share = models.BigIntegerField()
+    total_equity = models.BigIntegerField()
+    total_revenue = models.BigIntegerField()
+    gross_profit = models.BigIntegerField()
+    operating_profit = models.BigIntegerField()
+    interest_expense = models.BigIntegerField()
+    net_profit = models.BigIntegerField()
+    earnings_per_share = models.IntegerField()
+    dividend_payment = models.BigIntegerField()
+    stock_price = models.IntegerField()
+    kurs_rupiah_dollar = models.IntegerField()
+
+    def __str__(str):
+        return '%s %s'%(self.kd_perusahaan, self.year)
